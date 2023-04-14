6fd6de06c093e3554dc7e6fb94324608e762db93
JaimeCabreraDev <jacabrer@bancolombia.com.co>
2023-04-14 15:32:04 -0500

    Modificar variables de entorno
    Se modifican variables de entorno para la base de datos

--- config/database.py
+++ config/database.py
@@ -1,7 +1,15 @@
+from os import environ
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:w3jirnj0ctKBt9XmssFM@containers-us-west-131.railway.app:6130/railway"
+USER_DB = environ.get("USER_DB")
+PASSWORD_DB = environ.get("PASSWORD_DB")
+DATABASE_DB = environ.get("DATABASE_DB")
+HOST_DB = environ.get("HOST_DB")
+PORT_DB = environ.get("PORT_DB")
+
+DATABASE_URL = f"mysql+pymysql://{USER_DB}:{PASSWORD_DB}@{HOST_DB}:{PORT_DB}/{DATABASE_DB}"
 engine = create_engine(DATABASE_URL)
 
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
